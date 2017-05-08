dbb911fdd7c279b175fc5faf67041284b502d1c0
EVE DENISON <evedenison@gmail.com>
2017-05-08 10:15:43 -0700

    added .gitignore

---
+++ CrossPlatformToDoList/CrossPlatformToDoList/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CrossPlatformToDoList
+//
+//  Created by Eve Denison on 5/8/17.
+//  Copyright © 2017 Eve Denison. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CrossPlatformToDoList/CrossPlatformToDoList/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CrossPlatformToDoList
+//
+//  Created by Eve Denison on 5/8/17.
+//  Copyright © 2017 Eve Denison. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end

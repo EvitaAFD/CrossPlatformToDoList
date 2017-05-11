4ba4b10314e739c430568378e498e6fa239fe491
EVE DENISON <evedenison@gmail.com>
2017-05-11 11:33:52 -0700

    added watchConnectivity into project and began adding email retreival

--- CrossPlatformToDoList/CrossPlatformToDoList/AppDelegate.m
+++ CrossPlatformToDoList/CrossPlatformToDoList/AppDelegate.m
@@ -9,8 +9,11 @@
 #import "AppDelegate.h"
 
 @import Firebase;
+@import WatchConnectivity;
 
-@interface AppDelegate ()
+typedef void(^FirebaseCallback)(NSArray *allToDos);
+
+@interface AppDelegate () <WCSessionDelegate>
 
 @end
 
@@ -22,7 +25,32 @@
     
     [FIRApp configure];
     
+    [[WCSession defaultSession] setDelegate:self];
+    [[WCSession defaultSession] activateSession];
+    
     return YES;
+}
+
+-(void)startMonitoringToDoUpdates:(FirebaseCallback)callback {
+
+    FIRDatabaseReference *databaseReference = [[FIRDatabase database] reference];
+    
+    FIRUser *currentUser = [[FIRAuth auth] currentUser];
+    
+    FIRDatabaseReference *userReference = [[databaseReference child:@"users"] child:currentUser.uid];
+    
+    [[userReference child:@"todos"] observeEventType:FIRDataEventTypeValue withBlock:^(FIRDataSnapshot * _Nonnull snapshot) {
+        
+        NSMutableArray *todoDictionaries = [[NSMutableArray alloc] init];
+        
+        for (FIRDataSnapshot *todoReference in snapshot.children) {
+            [todoDictionaries addObject:todoReference.value];
+        }
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            callback(todoDictionaries.copy);
+        });
+    }];
 }
 
 
@@ -50,6 +78,13 @@
 
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+-(void)session:(WCSession *)session didReceiveMessage:(NSDictionary<NSString *,id> *)message replyHandler:(void (^)(NSDictionary<NSString *,id> * _Nonnull))replyHandler {
+
+    [self startMonitoringToDoUpdates:^(NSArray *allToDos) {
+        replyHandler(@{@"todos": allToDos});
+    }];
 }
 
 
